88fd9aa8097ef3bb8920b2111b95c8b4137665ff
Stabel,Jordy J <j.stabel@student.fontys.nl>
2019-03-10 14:25:28 +0100

    made the UI

---
+++ src/main/java/enums/Category.java
@@ -0,0 +1,7 @@
+package enums;
+
+public enum Category {
+    CAT,
+    DOG,
+    ITEM
+}
--- src/main/java/shop/Shop.java
+++ src/main/java/shop/Shop.java
@@ -1,4 +1,37 @@
 package shop;
 
-public class Shop {
+import animals.Animal;
+import other.Observer;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Shop implements Observer {
+
+    private List<ISellable> inventory = new ArrayList<>();
+
+    public List<ISellable> getInventory(){
+        return this.inventory;
+    }
+
+    public void addNewItem(String name, double price){
+        addToInventory(new Item(name, price));
+    }
+
+    public void removeItem(Item item){
+        removeFromInventory(item);
+    }
+
+    private void addToInventory(ISellable item){
+        inventory.add(item);
+    }
+
+    private void removeFromInventory(ISellable item){
+        inventory.remove(item);
+    }
+
+    @Override
+    public void update(Object object) {
+        addToInventory((Animal)object);
+        removeFromInventory((Item)object);
+    }
 }
